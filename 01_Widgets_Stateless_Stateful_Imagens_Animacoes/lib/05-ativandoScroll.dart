efbabd31bcac0715281a80e6ffb464013ad5e481
Flavio <flaviovmix@gmail.com>
2024-08-09 15:37:47 -0300

    CONTINUACAO DA AULA

---
+++ 01_Widgets_Stateless_Stateful_Imagens_Animacoes/lib/05-ativandoScroll.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tarefas'),
+          backgroundColor: const Color.fromARGB(255, 94, 131, 147),
+        ),
+        body: ListView(
+          //scrollDirection: Axis.horizontal,
+          children: [
+            Task('Curso Flutter'),
+            Task('Curso Java'),
+            Task('Curso HTML-5'),
+            Task('Curso Git'),
+            Task('Curso Git'),
+            Task('Curso Git'),
+            Task('Curso Git'),
+            Task('Curso Git'),
+            Task('Curso Git'),
+            Task('Curso Git'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        color: Colors.black, // nao sei porque o fundo nao ficou preto
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.black26,
+                    width: 72,
+                    height: 100,
+                  ),
+                  Container(
+                      width: 200,
+                      child: Text(nome,
+                          style: TextStyle(
+                              fontSize: 15, overflow: TextOverflow.ellipsis))),
+                  ElevatedButton(
+                      onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
